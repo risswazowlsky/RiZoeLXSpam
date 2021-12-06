a0bc06d887a9f491c3cc0cafad793332e181681a
risswazowlsky <85448152+risswazowlsky@users.noreply.github.com>
2021-12-07 05:30:30 +0700

    Update alive.py

--- RiZoeLXSpam/plugins/alive.py
+++ RiZoeLXSpam/plugins/alive.py
@@ -21,6 +21,7 @@ rizoel += f"┣➣ **ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ** : `3.9.6`\n"
 rizoel += f"┣➣ **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ** : `{version.__version__}`\n"
 
 rizoel += f"┣➣ **ᴠᴇʀsɪᴏɴ**  : `{rizoelversion}`\n"
+
     
 rizoel += f"┣➣ **Groups ᴍᴇ** : [Tap](https://t.me/CariKenalanBebas)\n"
 
